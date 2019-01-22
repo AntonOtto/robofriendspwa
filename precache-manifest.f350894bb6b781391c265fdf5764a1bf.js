a958fdfba9a2efc543245222578eddc98495c3aa
AntonOtto <antonotto81@gmail.com>
2019-01-22 19:04:12 +0200

    Updates

---
+++ precache-manifest.f350894bb6b781391c265fdf5764a1bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendspwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "761110f865759e97b3f9",
+    "url": "/robofriendspwa/static/js/runtime~main.761110f8.js"
+  },
+  {
+    "revision": "5205467332886312e3df",
+    "url": "/robofriendspwa/static/js/main.52054673.chunk.js"
+  },
+  {
+    "revision": "7cf20520a73ca2287dd1",
+    "url": "/robofriendspwa/static/js/1.7cf20520.chunk.js"
+  },
+  {
+    "revision": "5205467332886312e3df",
+    "url": "/robofriendspwa/static/css/main.8828cad2.chunk.css"
+  },
+  {
+    "revision": "7cf20520a73ca2287dd1",
+    "url": "/robofriendspwa/static/css/1.031bb0f1.chunk.css"
+  },
+  {
+    "revision": "4a300d939bae97ae4876f1a46ae4f150",
+    "url": "/robofriendspwa/index.html"
+  }
+];
